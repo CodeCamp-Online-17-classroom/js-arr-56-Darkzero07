88ea3b4818a889607632dcfb016df6d694296ef8
Darkzero7 <chaiyanituporn@gmail.com>
2024-06-23 21:44:22 +0700

    js lab 142

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-// write code here
+// จงหา unique array element ของ alphabets
+
+let alphabets = ["a", "b", "a", "b", "c", "e", "e", "c", "d", "d", "d", "d"];
+const unique = [...new Set(alphabets)];
+console.log(unique);
+// expected result: ['a', 'b', 'c', 'e', 'd']
